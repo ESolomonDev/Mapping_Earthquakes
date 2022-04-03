2ba2945c3cd8e5b2833d164dcca71ab61cb00b1e
Eli Solomon <ESolomonDev@gmail.com>
2022-04-03 18:52:14 -0400

    adding majorAirports.json

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,43 +1,46 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-
-let line2 = [
-  [40.7128, -74.0059],
-  [41.8781, -87.6298],
-  [29.7604, -95.3698],
-  [34.0522, -118.2437],
-  [33.4484, -112.0740]
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line2, {
-  color: "blue",
-  weight: 4,
-  opacity: 0.5
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+   }
 }).addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.circle([34.0522, -118.2437], {
-//   radius: 100
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.city + "</h2> </br> </h3>"+ feature.properties.country + "<h3>");
+
+//   }
+
 // }).addTo(map);
 
 // Get data from cities.js
